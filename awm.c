150d84536148c6d8df5210d422f145e0d016fc55
Ramon Fallon <ramonfallon@gmail.com>
2015-08-22 23:49:44 +0200

    prototype for mask created, to be used for marking off vertical columns

---
+++ awm.c
@@ -0,0 +1,32 @@
+/* array with mask type */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct
+{
+    int *a;
+    unsigned long *m;
+} awm_t;
+
+int main(int argc, char *argv[])
+{
+   /* argument accounting: remember argc, the number of arguments, _includes_ the executable */
+	if(argc!=2) {
+		printf("Error. Pls supply argument (name of file).\n");
+		exit(EXIT_FAILURE);
+	}
+
+   /* declarations */
+   FILE 
+   char
+   int
+   float
+   double
+
+   /* print stuff out */
+   printf("Res_1: %s\n", "somedata");
+
+   return 0;
+}
